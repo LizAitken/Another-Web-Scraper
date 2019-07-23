e642a93c755cb1fed18bd07619f44e43cf51c990
Liz Aitken <liz.a.aitken@gmail.com>
2019-07-23 07:17:43 -0400

    To write to a csv file

--- scrape.js
+++ scrape.js
@@ -1,4 +1,26 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('http://codedemos.com/sampleblog') 
+console.log('First console log');
+
+request('http://codedemos.com/sampleblog', (error, response, html) => {
+    if(!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+        console.log('starting');
+        const siteHeading = $('.site-heading');
+        // console.log(siteHeading.html());
+        // const output = siteHeading.find('h1').text();
+        // const output = siteHeading.children('h1').text();
+        // const output = siteHeading
+        //     .children('h1')
+        //     .next()
+        //     .text();
+        $('.nav-item a').each((i, el) => {
+            const item = $(el).text();
+            const link = $(el).attr('href');
+            console.log(item);
+            console.log(link);
+        });
+    }
+});
+
